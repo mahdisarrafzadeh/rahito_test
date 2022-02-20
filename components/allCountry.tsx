1022ff6167bde8a8f0dad85b3803dcb366db0ba2
mahdi <mahdisarrafz9@gmail.com>
2022-02-20 20:44:54 +0330

    create component

---
+++ components/allCountry.tsx
@@ -0,0 +1,76 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { useQuery } from 'react-query';
+import CountryService from '../services/TutorialService';
+import { allCountry } from '../types/Country';
+import StarRatingComponent from 'react-star-rating-component';
+import { GlobalContext } from '../context/GlobalState';
+import handler from '../pages/api/hello';
+
+
+type Props = {}
+
+export const AllCountry = (props: Props) => {
+    const { getAllCompany, updateCountry, country } = useContext(GlobalContext);
+    const [getResult, setGetResult] = useState<allCountry[]>();
+    const [first, setfirst] = useState<any[]>([])
+    const { isLoading: isLoadingTutorials, refetch: getAllTutorials } = useQuery<allCountry[], Error>(
+        "query-tutorials",
+        async () => {
+            return await CountryService.findAllCountry();
+        },
+        {
+            enabled: false,
+            onSuccess: (res) => {
+                getAllCompany(res);
+            },
+            onError: (err: any) => {
+                setGetResult(err);
+            },
+        }
+    );
+    function getAllData() {
+        try {
+            getAllTutorials();
+        } catch (err: any) {
+            setGetResult(err);
+        }
+    }
+    useEffect(() => {
+        getAllData();
+    }, []);
+    const handleStar = (name: string, cioc: string) => {
+        updateCountry(name, cioc)
+        setfirst(prev => [...prev, name])
+    }
+
+
+    useEffect(() => {
+        if (first.length !== 0) {
+            localStorage.setItem("name", JSON.stringify(first))
+        } else {
+            console.log(first)
+        }
+        return () => {
+        }
+    }, [first])
+
+    return (
+        <div> <div className='flex flex-col h-96 items-center overflow-auto'>
+            {
+                country?.map((post: any, index: any) => {
+                    return <div key={index} className="w-48 p-2 flex items-center justify-between" >
+                        <h2 className='cursor-pointer' onClick={() => handleStar(post.name, post.cioc)}>{post.name}</h2><div>
+                            <StarRatingComponent
+                                name="rate1"
+                                starCount={1}
+                                editing={false}
+                                value={post.clicks ? 1 : 0}
+                            /></div>
+                    </div>
+                })
+            }
+        </div></div>
+    )
+}
+
+//
---
+++ components/flag.tsx
@@ -0,0 +1,54 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { useQuery } from 'react-query';
+import { GlobalContext } from '../context/GlobalState';
+import CountryService from '../services/TutorialService';
+import { Country } from '../types/Country';
+
+type Props = {}
+
+export const Flag = (props: Props) => {
+    const { countryFlag, cioc, country_flag } = useContext(GlobalContext);
+
+    const [getCountry, setCountry] = useState<Country[]>();
+    const [Statecioc, setCioc] = useState<string>("")
+
+    const { isLoading: isLoadingTutorial, refetch: getTutorialById } = useQuery<Country[], Error>(
+        "query-tutorial-by-id",
+        async () => {
+            return await CountryService.findByCioc(cioc);
+        },
+        {
+            enabled: false,
+            retry: 1,
+            onSuccess: (res) => {
+                countryFlag(res);
+            },
+            onError: (err: any) => {
+                setCountry(err.response?.data || err);
+            },
+        }
+    );
+
+    function getDataById() {
+        if (cioc) {
+            try {
+                getTutorialById();
+            } catch (err: any) {
+                setCountry(err);
+            }
+        }
+    }
+
+
+    useEffect(() => {
+        console.log(cioc)
+        getDataById()
+
+    }, [cioc])
+
+    return (
+        <React.Fragment>
+            {country_flag?.map((item: any, index: any) => <img key={item.flags.png} className="flex " src={item.flags.png} />)}
+        </React.Fragment>
+    )
+}
