8fad851e88c5ae70b9d4904d6db11d6d8dca07e1
mahdi <mahdisarrafz9@gmail.com>
2022-02-21 09:48:44 +0330

    click and loading handle

--- components/allCountry.tsx
+++ components/allCountry.tsx
@@ -11,7 +11,7 @@ type Props = {}
 
 export const AllCountry = (props: Props) => {
     const { getAllCompany, updateCountry, country } = useContext(GlobalContext);
-    const [getResult, setGetResult] = useState<allCountry[]>();
+    const [getResult, setGetResult] = useState<string>();
     const [first, setfirst] = useState<any[]>([])
     const { isLoading: isLoadingTutorials, refetch: getAllTutorials } = useQuery<allCountry[], Error>(
         "query-tutorials",
@@ -28,6 +28,12 @@ export const AllCountry = (props: Props) => {
             },
         }
     );
+
+    useEffect(() => {
+        if (isLoadingTutorials) setGetResult("loading...");
+    }, [isLoadingTutorials]);
+
+
     function getAllData() {
         try {
             getAllTutorials();
@@ -39,6 +45,7 @@ export const AllCountry = (props: Props) => {
         getAllData();
     }, []);
     const handleStar = (name: string, cioc: string) => {
+        setGetResult(name)
         updateCountry(name, cioc)
         setfirst(prev => [...prev, name])
     }
@@ -51,24 +58,43 @@ export const AllCountry = (props: Props) => {
         return () => {
         }
     }, [first])
+    if (isLoadingTutorials) {
+        return (
+            <div>
+                ....loading
+            </div>
+        )
 
-    return (
-        <div> <div className='flex flex-col h-96 items-center overflow-auto'>
-            {
-                country?.map((post: any, index: any) => {
-                    return <div key={index} className="w-48 p-2 flex items-center justify-between" >
-                        <h2 className='cursor-pointer' onClick={() => handleStar(post.name, post.cioc)}>{post.name}</h2><div>
-                            <StarRatingComponent
-                                name="rate1"
-                                starCount={1}
-                                editing={false}
-                                value={post.clicks ? 1 : 0}
-                            /></div>
-                    </div>
-                })
-            }
-        </div></div>
-    )
+    } else {
+
+        return (
+            <div>
+                <div className='flex flex-col shadow-2xl  rounded    h-96 items-center overflow-auto'>
+                    {
+                        country?.map((post: any, index: any) => {
+                            return <div key={index} onClick={() => handleStar(post.name, post.cioc)} className={` cursor-pointer p-4 border hover:bg-slate-300 border-gray-200 text-gray-500 flex w-full justify-between ${getResult === post.name && "bg-red-600 text-white"} `}>
+                                <span className='inline-flex text-xl font-semibold items-center space-x-5'>
+                                    <StarRatingComponent
+                                        name="rate1"
+                                        emptyStarColor={"#6B8072"}
+                                        starCount={1}
+                                        editing={false}
+                                        value={post.clicks ? 1 : 0}
+                                    />
+                                    <h2 className='cursor-pointer text-base '> {post.name}</h2>
+                                </span>
+
+                                <div>
+                                    <h1 className='font-thin text-sm'>
+                                        {post.cioc}
+                                    </h1>
+                                </div>
+                            </div>
+                        })
+                    }
+                </div></div>
+        )
+    }
 }
 
 //
